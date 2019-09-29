a411a89b72f85faecebf146d5ecebc71a996d30a
Nagy Roland <n.roland07@gmail.com>
2019-09-29 19:36:51 +0200

    End of section 6.

--- GameOfRooms-SPA/src/app/register/register.component.ts
+++ GameOfRooms-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,24 +11,26 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  register(){
+  register() {
     this.authService.register(this.model).subscribe(
-      ()=>{
-        console.log('Registration successfull!');
-      }, error => {
-        console.log(error);
+      () => {
+        this.alertify.success('Registration successfull!');
+      },
+      error => {
+        this.alertify.error(error);
       }
     );
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
     console.log('register cancelled');
   }
-
 }
